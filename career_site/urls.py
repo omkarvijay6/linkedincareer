5f97ffd4ab48c95ae05075f71782a69ea5acaa18
omkarvijay6 <omkarvijay6@gmail.com>
2016-06-01 23:14:07 +0530

    about page, url, templates, css, static

--- career_site/urls.py
+++ career_site/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from . import views
+from views import index, about
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', index, name='index'),
+    url(r'^about/', about, name='about'),
 ]
--- career_site/views.py
+++ career_site/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render_to_response, RequestContext
 
 def index(request):
     return render_to_response('index.html', {}, context_instance=RequestContext(request))
+
+
+def about(request):
+    return render_to_response('about.html', {}, context_instance=RequestContext(request))

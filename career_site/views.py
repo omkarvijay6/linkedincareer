2cc26759e09ebc3f1564021938e033c95dee9bd6
omkarvijay6 <omkarvijay6@gmail.com>
2016-06-01 23:58:53 +0530

    fixed header current tab

--- career_site/views.py
+++ career_site/views.py
@@ -4,17 +4,25 @@ from django.shortcuts import render_to_response, RequestContext
 # Create your views here.
 
 def index(request):
-    return render_to_response('index.html', {}, context_instance=RequestContext(request))
+    current_url = request.path
+    return render_to_response('index.html', {'index_url': current_url}, context_instance=RequestContext(request))
 
 
 def about(request):
-    return render_to_response('about.html', {}, context_instance=RequestContext(request))
+    current_url = request.path
+    return render_to_response('about.html', {'about_url': current_url}, context_instance=RequestContext(request))
+
 
 def services(request):
-    return render_to_response('services.html', {}, context_instance=RequestContext(request))
+    current_url = request.path
+    return render_to_response('services.html', {'services_url': current_url}, context_instance=RequestContext(request))
+
 
 def news(request):
-    return render_to_response('news.html', {}, context_instance=RequestContext(request))
+    current_url = request.path
+    return render_to_response('news.html', {'news_url': current_url}, context_instance=RequestContext(request))
+
 
 def contact(request):
-    return render_to_response('contact.html', {}, context_instance=RequestContext(request))
+    current_url = request.path
+    return render_to_response('contact.html', {'contact_url': current_url}, context_instance=RequestContext(request))

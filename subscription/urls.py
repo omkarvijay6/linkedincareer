31d5c1fd78708477eed6c27a58ef2fc320123cf7
omkarvijay6 <omkarvijay6@gmail.com>
2016-08-29 13:17:45 +0530

    subscription payment static template and cuntionality

--- subscription/urls.py
+++ subscription/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from subscription.views import payment_callback
+from subscription.views import payment_callback, payment
 
 urlpatterns = [
     url(r'^payment-callback/$', payment_callback, name='payment_callback'),
+    url(r'^payment/$', payment, name='payment'),
 
 ]
--- subscription/views.py
+++ subscription/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render_to_response
 from django.core.urlresolvers import reverse
 
 
 # Create your views here.
+from django.template import RequestContext
+
 
 def payment_callback(request):
     return redirect(reverse('index'))
+
+@login_required
+def payment(request):
+    return render_to_response('payment.html', {}, context_instance=RequestContext(request))

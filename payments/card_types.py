2f89ecab87e76cc91a8b51a2d2935559733ada76
omkarvijay6 <omkarvijay6@gmail.com>
2016-09-15 23:57:25 +0530

    card types

---
+++ payments/card_types.py
@@ -0,0 +1,41 @@
+AE = 'AE'
+AP = 'AP'
+DC = 'DC'
+GC = 'GC'
+XX = 'XX'
+JC = 'JC'
+LY = 'LY'
+MS = 'MS'
+MC = 'MC'
+MX = 'MX'
+PL = 'PL'
+SD = 'SD'
+SO = 'SO'
+ST = 'ST'
+SW = 'SW'
+VD = 'VD'
+VC = 'VC'
+VP = 'VP'
+EB = 'EB'
+
+CREDIT_CARD_TYPES = (
+    (AE, 'American Express'),
+    (AP, 'American Express Corporate Purchase Card'),
+    (DC, 'Diners Club'),
+    (GC, 'GAP Inc. card'),
+    (XX, 'Generic Card'),
+    (JC, 'JCB Card'),
+    (LY, 'Loyalty Card'),
+    (MS, 'Maestro Card'),
+    (MC, 'MasterCard'),
+    (MX, 'Mondex Card'),
+    (PL, 'PLC Card'),
+    (SD, 'SafeDebit Card'),
+    (SO, 'SOLO Card'),
+    (ST, 'STYLE Card'),
+    (SW, 'SWITCH Card'),
+    (VD, 'Visa Debit Card'),
+    (VC, 'Visa Card'),
+    (VP, 'Visa Corporate Purchase Card'),
+    (EB, 'Electronic Benifit Card'),
+)

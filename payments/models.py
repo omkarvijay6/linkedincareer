5366e9409b39bcd9a8d9bf4b2db3a120e121b4fb
omkarvijay6 <omkarvijay6@gmail.com>
2016-09-15 23:57:04 +0530

    Payment and Order model

--- payments/models.py
+++ payments/models.py
@@ -1,5 +1,78 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+from payments.card_types import CREDIT_CARD_TYPES
+from payments.services import generate_unique_merch_txn_ref
+
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self-updating ''created_ts''
+    and ''updated_ts'' timestamp fields.
+    """
+    created_ts = models.DateTimeField('created', auto_now_add=True)
+    updated_ts = models.DateTimeField('updated', auto_now=True)
+    is_active = models.BooleanField(default=True, db_index=True)
+
+    class Meta:
+        abstract = True
+
+
+class Order(TimeStampedModel):
+    """
+    defines the order of the services (ex: resume writing, right connect etc)
+    For now order does not have any lineitems.
+    Each transaction and order is independent
+    name: name of the service for which order has to be created
+    merch_txn_ref: Unique Merchant Transaction Reference number which is sent to the
+    payment server which checkout
+    """
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    merch_txn_ref = models.CharField(unique=True, max_length=40, help_text="Merchant Transaction Reference")
+
+    def save(self, *args, **kwargs):
+        """
+        override save method to create an unique merchant transaction reference
+        """
+        if self.pk is None:
+            self.merch_txn_ref = generate_unique_merch_txn_ref()
+        super(Order, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Payment(TimeStampedModel):
+    """
+    batch_scheduled_date: date at which the actual bank transaction takes place
+    status: Pending, rejected, approved depends on the status of the transaction
+    status_message: proper message relating to the status
+    transaction_no: unique transaction identifier which is used for admin reference
+    card_type: card_type of the user
+    reciept_no: unique identifier for which is used for the user reference
+    """
+    batch_scheduled_date = models.DateField(blank=True, null=True)
+
+    PENDING = 'PENDING'
+    REJECTED = 'REJECTED'
+    APPROVED = 'APPROVED'
+    STATUS_CHOICES = (
+        (PENDING, 'PENDING'),
+        (REJECTED, 'REJECTED'),
+        (APPROVED, 'APPROVED'),
+    )
+    status = models.CharField(choices=STATUS_CHOICES, max_length=8, default=PENDING)
+    status_message = models.TextField(null=True, blank=True)
+    transaction_num = models.CharField(unique=True, max_length=19, null=True, blank=True,
+                                       help_text="Transaction Number")
+    card_type = models.CharField(choices=CREDIT_CARD_TYPES, max_length=2, null=True, blank=True)
+    receipt_no = models.CharField(unique=True, max_length=12, null=True, blank=True, help_text="Receipt Number")
+    order = models.ForeignKey('payments.Order')
+
+    def __unicode__(self):
+        unicode_str = self.transaction_num + self.status if self.transaction_num else self.status
+        return unicode_str

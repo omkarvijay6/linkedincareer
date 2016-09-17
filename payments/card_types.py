e2d62a6e829af581475253394f6580d6b41746de
omkarvijay6 <omkarvijay6@gmail.com>
2016-09-17 18:31:06 +0530

    payment card types to be as enums

--- payments/card_types.py
+++ payments/card_types.py
@@ -1,41 +1,23 @@
-AE = 'AE'
-AP = 'AP'
-DC = 'DC'
-GC = 'GC'
-XX = 'XX'
-JC = 'JC'
-LY = 'LY'
-MS = 'MS'
-MC = 'MC'
-MX = 'MX'
-PL = 'PL'
-SD = 'SD'
-SO = 'SO'
-ST = 'ST'
-SW = 'SW'
-VD = 'VD'
-VC = 'VC'
-VP = 'VP'
-EB = 'EB'
+from payments.enums import CardTypes
 
 CREDIT_CARD_TYPES = (
-    (AE, 'American Express'),
-    (AP, 'American Express Corporate Purchase Card'),
-    (DC, 'Diners Club'),
-    (GC, 'GAP Inc. card'),
-    (XX, 'Generic Card'),
-    (JC, 'JCB Card'),
-    (LY, 'Loyalty Card'),
-    (MS, 'Maestro Card'),
-    (MC, 'MasterCard'),
-    (MX, 'Mondex Card'),
-    (PL, 'PLC Card'),
-    (SD, 'SafeDebit Card'),
-    (SO, 'SOLO Card'),
-    (ST, 'STYLE Card'),
-    (SW, 'SWITCH Card'),
-    (VD, 'Visa Debit Card'),
-    (VC, 'Visa Card'),
-    (VP, 'Visa Corporate Purchase Card'),
-    (EB, 'Electronic Benifit Card'),
+    (CardTypes.american_express_card.value, 'American Express'),
+    (CardTypes.american_express_purchase_card.value, 'American Express Corporate Purchase Card'),
+    (CardTypes.diners_club_card.value, 'Diners Club'),
+    (CardTypes.gap_inc_card.value, 'GAP Inc. card'),
+    (CardTypes.generic_card.value, 'Generic Card'),
+    (CardTypes.jcb_card.value, 'JCB Card'),
+    (CardTypes.loyalty_card.value, 'Loyalty Card'),
+    (CardTypes.maestro_card.value, 'Maestro Card'),
+    (CardTypes.master_card.value, 'MasterCard'),
+    (CardTypes.mondex_card.value, 'Mondex Card'),
+    (CardTypes.plc_card.value, 'PLC Card'),
+    (CardTypes.safe_debit_card.value, 'SafeDebit Card'),
+    (CardTypes.solo_card.value, 'SOLO Card'),
+    (CardTypes.style_card.value, 'STYLE Card'),
+    (CardTypes.switch_card.value, 'SWITCH Card'),
+    (CardTypes.visa_debit_card.value, 'Visa Debit Card'),
+    (CardTypes.visa_card.value, 'Visa Card'),
+    (CardTypes.visa_corporate_purchase_card.value, 'Visa Corporate Purchase Card'),
+    (CardTypes.electronic_benifit_card.value, 'Electronic Benifit Card'),
 )

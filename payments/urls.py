66d3dcefea15c101aa097807a9fc632d8a6b53db
omkarvijay6 <omkarvijay6@gmail.com>
2016-09-17 23:36:43 +0530

    updated the templates

--- payments/urls.py
+++ payments/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^redirect/payment-gateway/(?P<amount>\d+)/(?P<service_nk>\w+)/(?P<country_code>\w+)/$', redirect_to_payment_gateway, name='payment_with_country'),
     url(r'^redirect/payment-gateway/(?P<amount>\d+)/(?P<service_nk>\w+)/$', redirect_to_payment_gateway, name='payment_without_country'),
     url(r'^successful/$', TemplateView.as_view(template_name="payment_successful.html"), name="payment_successful"),
-    url(r'^failed/$', TemplateView.as_view(template_name="payment_failure.html"), name="payment_failed"),
+    url(r'^failed/$', TemplateView.as_view(template_name="payment_failed.html"), name="payment_failed"),
     url(r'^forbidden/$', TemplateView.as_view(template_name="forbidden.html"), name="forbidden"),
 
 ]

7ec314fc5c0aad327c2ffd40455e3d8465a700ed
omkarvijay6 <omkarvijay6@gmail.com>
2016-09-17 18:30:06 +0530

    change url name redirect_to_payment_gateway

--- payments/urls.py
+++ payments/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from payments.views import redirect_to_payment_gateway
 
 urlpatterns = [
-    url(r'^redirect/payment-gateway/$', redirect_to_payment_gateway, name='payment'),
+    url(r'^redirect/payment-gateway/(?P<amount>\d+)/$', redirect_to_payment_gateway, name='payment'),
 
 ]

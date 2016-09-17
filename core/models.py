f009f28b2871ee0c94e701f3e15556a425e9ff44
omkarvijay6 <omkarvijay6@gmail.com>
2016-09-17 19:27:35 +0530

    moved timestamped to core

--- core/models.py
+++ core/models.py
@@ -2,4 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self-updating ''created_ts''
+    and ''updated_ts'' timestamp fields.
+    """
+    created_ts = models.DateTimeField('created', auto_now_add=True)
+    updated_ts = models.DateTimeField('updated', auto_now=True)
+    is_active = models.BooleanField(default=True, db_index=True)
+
+    class Meta:
+        abstract = True

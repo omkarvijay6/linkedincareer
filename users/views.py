dd07592a8168aeb9a01f8665a22b91599a0061f7
omkarvijay6 <omkarvijay6@gmail.com>
2016-09-19 11:28:00 +0530

    fixed next issue

--- users/views.py
+++ users/views.py
@@ -8,17 +8,22 @@ from users.forms import UserLoginForm
 
 def user_login(request):
     if request.user.is_authenticated():
-        nextpage = request.GET.get('next', reverse('index'))
+        nextpage = request.session.get('next', reverse('index'))
         return HttpResponseRedirect(nextpage)
     auth_form = UserLoginForm()
     if request.method == 'POST':
-        nextpage = request.GET.get('next', reverse('index'))
+        nextpage = request.session.get('next', None)
+        if nextpage:
+            del request.session['next']
+        else:
+            nextpage = reverse('index')
         auth_form = UserLoginForm(data=request.POST)
         if auth_form.is_valid():
             login(request, auth_form.get_user())
             return HttpResponseRedirect(nextpage)
         else:
             return render_to_response('login.html', {'auth_form': auth_form}, RequestContext(request))
+    request.session['next'] = request.GET.get('next')
     return render_to_response('login.html', {'auth_form': auth_form}, RequestContext(request))
 
 

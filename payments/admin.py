6cdc2c65dabdda03e5e8b2a14720b3837ed927e0
omkarvijay6 <omkarvijay6@gmail.com>
2016-09-19 11:10:21 +0530

    updated_ts and created_ts in admin

--- payments/admin.py
+++ payments/admin.py
@@ -7,9 +7,9 @@ from payments.models import Order, Payment
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ('batch_scheduled_date', 'status', 'status_message', 'transaction_num',
-                    'card_type', 'receipt_no', 'order')
+                    'card_type', 'receipt_no', 'order', 'updated_ts', 'created_ts',)
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'merch_txn_ref', 'amount', 'country',)
+    list_display = ('title', 'user', 'merch_txn_ref', 'amount', 'country', 'updated_ts', 'created_ts')

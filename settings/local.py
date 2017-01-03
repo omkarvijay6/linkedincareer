97aebe26734a9d1a58eb66d70b038569f8da8084
omkarvijay5 <omkarvijay5@gmail.com>
2017-01-03 18:58:33 +0530

    changed paypal settings depending on prod and local

--- settings/local.py
+++ settings/local.py
@@ -1,4 +1,6 @@
 from settings.base import *
 
+PAYPAL_TEST = True
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- settings/prod.py
+++ settings/prod.py
@@ -1,5 +1,7 @@
 from settings.base import *
 DEBUG = False
 
+PAYPAL_TEST = False
+
 SECURE_SSL_REDIRECT = True # [1]
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

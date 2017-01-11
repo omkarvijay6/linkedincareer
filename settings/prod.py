f4be705dfd7607e4a7c80bf95f56aceeed3f2e15
omkarvijay5 <omkarvijay5@gmail.com>
2017-01-11 12:17:34 +0530

    allowed hosts * for prod

--- settings/prod.py
+++ settings/prod.py
@@ -2,6 +2,7 @@ from settings.base import *
 DEBUG = False
 
 PAYPAL_TEST = False
+ALLOWED_HOSTS = ['*']
 
 SECURE_SSL_REDIRECT = True # [1]
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

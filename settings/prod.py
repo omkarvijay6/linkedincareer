884bbfdf000523ff6bfd0000ea94cbba02102b7a
omkarvijay6 <omkarvijay6@gmail.com>
2017-01-16 15:40:12 +0530

    comment out the redirect to https

--- settings/prod.py
+++ settings/prod.py
@@ -4,5 +4,6 @@ DEBUG = False
 PAYPAL_TEST = False
 ALLOWED_HOSTS = ['*']
 
-SECURE_SSL_REDIRECT = True # [1]
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# not required as it is handled by nginx
+# SECURE_SSL_REDIRECT = True # [1]
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
